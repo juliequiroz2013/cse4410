18d1b0a00469941648eb6c983ec82afcbb82511f
inguanzod <inguanzod@gmail.com>
2015-05-05 21:10:34 -0500

    update

---
+++ Tabs/app/src/main/java/example/com/tabs/myContext.java
@@ -0,0 +1,14 @@
+package example.com.tabs;
+
+import android.content.Context;
+
+/**
+ * Created by David on 5/5/2015.
+ */
+public class myContext {
+    Context c;
+    public myContext(Context context)
+    {
+        c = context;
+    }
+}
